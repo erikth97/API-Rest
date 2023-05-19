9c733c5d988931daeff7dca5417597ddfa20f904
Erik Tamayo <erik.tamayohe@uanl.edu.mx>
2023-05-19 11:13:36 -0600

    Create ProductController.php

---
+++ src/Controllers/ProductController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Config\ResponseHttp;
+use App\Config\Security;
+use App\Models\ProductModel;
+
+class ProductController extends BaseController{   
+
+    final public function getAll(string $endPoint)
+    {
+        if ($this->getMethod() == 'get' && $endPoint == $this->getRoute()) {
+            Security::validateTokenJwt(Security::secretKey());
+            echo json_encode(ProductModel::getAll());
+            exit;
+        }    
+    }
+
+    final public function postSave(string $endPoint)
+    {
+        if ($this->getMethod() == 'post' && $endPoint == $this->getRoute()) {
+            Security::validateTokenJwt(Security::secretKey());
+            
+            if (empty($this->getParam()['name']) || empty($this->getParam()['description']) || empty($this->getParam()['stock']) || empty($_FILES['product'])) {
+                echo json_encode(ResponseHttp::status400('Todos los campos son requeridos'));
+            } else if(!preg_match(self::$validate_text,$this->getParam()['name'])) {
+                echo json_encode(ResponseHttp::status400('El campo Nombre solo admite texto'));
+            } else if(!preg_match(self::$validate_description,$this->getParam()['description'])) {
+                echo json_encode(ResponseHttp::status400('El campo Descripción solo admite texto y un máximo de 30 caracteres'));
+            } else if(!preg_match(self::$validate_stock,$this->getParam()['stock'])) {
+                echo json_encode(ResponseHttp::status400('El campo Stock solo admite números'));
+            } else {
+                new ProductModel($this->getParam(),$_FILES);
+                echo json_encode(ProductModel::postSave());
+            }
+            exit;
+        }
+    }
+
+    final public function delete(string $endPoint)
+    {
+        if ($this->getMethod() == 'delete' && $endPoint == $this->getRoute()) {
+            Security::validateTokenJwt(Security::secretKey());
+            
+            if (empty($this->getParam()['name']) || empty($this->getParam()['IDtoken'])) {
+                echo json_encode(ResponseHttp::status400('Todos los campos son requeridos'));
+            } else {
+                ProductModel::setImageName($this->getParam()['name']);
+                ProductModel::setIDtoken($this->getParam()['IDtoken']);
+                echo json_encode(ProductModel::delete());
+            }
+            exit;
+        }
+    }
+}
