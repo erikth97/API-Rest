eeff3773a263a155d5bdf04f8bbc9f6ecc5e62e4
Erik Tamayo <erik.tamayohe@uanl.edu.mx>
2023-05-23 14:27:53 -0600

    Update auth.php

--- src/Routes/auth.php
+++ src/Routes/auth.php
@@ -1,3 +1,16 @@
 <?php
 
-echo 'Hola desde auth';
+use App\Config\ResponseHttp;
+use App\Controllers\UserController;
+
+$method = strtolower($_SERVER['REQUEST_METHOD']);
+$route = $_GET['route'];
+$params = explode('/', $route);
+$data = json_decode(file_get_contents("php://input"),true);
+$headers = getallheaders();
+
+$app = new UserController($method,$route,$params,$data,$headers);
+
+$app->getLogin("auth/{$params[1]}/{$params[2]}");
+
+echo json_encode(ResponseHttp::status400());
