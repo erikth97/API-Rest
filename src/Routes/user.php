9a510361cb9605aa455dce5d84486ccda7df4f6a
Erik Tamayo <erik.tamayohe@uanl.edu.mx>
2023-05-23 14:28:15 -0600

    Update user.php

--- src/Routes/user.php
+++ src/Routes/user.php
@@ -1,3 +1,16 @@
 <?php
 
-echo 'Hola desde user';
+use App\Config\ResponseHttp;
+use App\Controllers\UserController;
+
+$method = strtolower($_SERVER['REQUEST_METHOD']);
+$route = $_GET['route'];
+$params = explode('/', $route);
+$data = json_decode(file_get_contents("php://input"),true);
+$headers = getallheaders();
+
+$app = new UserController($method,$route,$params,$data,$headers);
+
+$app->post('user/');
+
+echo json_encode(ResponseHttp::status400());
