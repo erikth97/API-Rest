d24df25cf2e870ce8ef2d95931c0db7382689180
Erik Tamayo <erik.tamayohe@uanl.edu.mx>
2023-05-23 14:30:14 -0600

    Update UserController.php

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -3,122 +3,66 @@
 namespace App\Controllers;
 
 use App\Config\ResponseHttp;
-use App\Config\Security;
 use App\Models\UserModel;
-use Rakit\Validation\Validator;
 
-class UserController extends BaseController{   
+class UserController
+{
+    private static $validate_rol = '/^[1,2,3]{1,1}$/';
+    private static $validate_number = '/^[0-9]+$/';
+    private static $validate_text = '/^[a-zA-Z]+$/';
+
+    public function __construct(
+        private string $method,
+        private string $route,
+        private array $params,
+        private $data,
+        private $headers
+    ) 
+    {}
 
     final public function getLogin(string $endPoint)
-    { 
-        if ($this->getMethod() == 'get' && $endPoint == $this->getRoute()) {
-            $email = strtolower($this->getAttribute()[1]);
-            $password = $this->getAttribute()[2];
+    {
+        if ($this->method == 'get' && $endPoint == $this->route) {
+            $email = strtolower($this->params[1]);
+            $password = $this->params[2];
 
-            if(empty($email) || empty($password)){  
-                echo json_encode(ResponseHttp::status400('Todos los campos son necesarios'));
-            }else if(!self::validateEmail($email)){
+            if(empty($email) || empty($password)){
+                echo json_encode(ResponseHttp::status400('Todos los campos son ncesesarios'));
+            }else if(!filter_var($email , FILTER_VALIDATE_EMAIL)){
                 echo json_encode(ResponseHttp::status400('Formato de correo invalido'));
             }else{
                 UserModel::setEmail($email);
                 UserModel::setPassword($password);
-                echo json_encode(UserModel::login());           
-            }  
-        exit;
-        }
-    }
-
-    final public function getAll(string $endPoint)
-    {
-        if ($this->getMethod() == 'get' && $endPoint == $this->getRoute()) {
-            Security::validateTokenJwt(Security::secretKey());
-            echo json_encode(UserModel::getAll());
-            exit;
-        }    
-    }
-
-    final public function getUser(string $endPoint)
-    {
-        if ($this->getMethod() == 'get' && $endPoint == $this->getRoute()) {
-            Security::validateTokenJwt(Security::secretKey());
-            $dni = $this->getAttribute()[1];
-            if (!isset($dni)) {
-                echo json_encode(ResponseHttp::status400('El campo DNI es requerido'));
-            }else if (!preg_match(self::$validate_number, $dni)) {
-                echo json_encode(ResponseHttp::status400('El DNI soo admite números'));
-            } else {
-                UserModel::setDni($dni);
-                echo json_encode(UserModel::getUser());
-                exit;
-            }  
-            exit;
-        }    
-    }
-
-    final public function postSave(string $endPoint)
-    {
-       if ($this->getMethod() == 'post' && $endPoint == $this->getRoute()) {
-
-
-        $validator = new Validator;
-        
-        $validation = $validator->validate($this->getParam(), [
-            'name'               => 'required|regex:/^[a-zA-Z ]+$/',
-            'dni'                => 'required|numeric',
-            'email'              => 'required|email',            
-            'rol'                => 'required|numeric|min:1|regex:/^[12]+$/',
-            'password'           => 'required|min:8',
-            'confirmPassword'    => 'required|same:password'   
-        ]);      
-
-        if ($validation->fails()) {            
-            $errors = $validation->errors();            	
-            echo json_encode(ResponseHttp::status400($errors->all()[0]));
-        } else {            
-            new UserModel($this->getParam());
-            echo json_encode(UserModel::postSave());
-        }              
-                          
-        exit;
-       }
-    }   
-
-    final public function patchPassword(string $endPoint)
-    {        
-        if ($this->getMethod() == 'patch' && $this->getRoute() == $endPoint){            
-            Security::validateTokenJwt(Security::secretKey());
-
-            $jwtUserData = Security::getDataJwt();                  
-
-            if (empty($this->getParam()['oldPassword']) || empty($this->getParam()['newPassword']) || empty($this->getParam()['confirmNewPassword'])) {
-                echo json_encode(ResponseHttp::status400('Todos los campos son requeridos'));
-            } else if (!UserModel::validateUserPassword($jwtUserData['IDToken'],$this->getParam()['oldPassword'])) {
-                echo json_encode(ResponseHttp::status400('La contraseña antigua no coincide'));
-            } else if (strlen($this->getParam()['newPassword']) < 8 || strlen($this->getParam()['confirmNewPassword']) < 8 ) {
-                echo json_encode(ResponseHttp::status400('La contraseña debe tener un minimo de 8 caracteres'));
-            }else if ($this->getParam()['newPassword'] !== $this->getParam()['confirmNewPassword']){
-                echo json_encode(ResponseHttp::status400('Las contraseñas no coinciden'));
-            } else {
-                UserModel::setIDToken($jwtUserData['IDToken']);
-                UserModel::setPassword($this->getParam()['newPassword']); 
-                echo json_encode(UserModel::patchPassword());
-            } 
-            exit;
-        }        
-    } 
-     
-    final public function deleteUser(string $endPoint)
-    {
-        if ($this->getMethod() == 'delete' && $this->getRoute() == $endPoint){
-            Security::validateTokenJwt(Security::secretKey());
-
-            if (empty($this->getParam()['IDToken'])) {
-                echo json_encode(ResponseHttp::status400('Todos los campos son requeridos'));
-            } else {
-                UserModel::setIDToken($this->getParam()['IDToken']);
-                echo json_encode(UserModel::deleteUser());
+                echo json_encode(UserModel::login());
             }
         exit;
         }
     }
+
+    final public function post(string $endPoint)
+    {
+        if ($this->method == 'post' && $endPoint == $this->route) {
+            if (empty($this->data['name']) || empty($this->data['dni']) || empty($this->data['email']) ||
+            empty($this->data['rol']) || empty($this->data['password']) || empty($this->data['confirmPassword'])) {
+                echo json_encode(ResponseHttp::status400('Todos los campos son requeridos'));
+            } else if (!preg_match(self::$validate_text, $this->data['name'])) {
+                echo json_encode(ResponseHttp::status400('El campo nombre solo admite texto'));
+            } else if (!preg_match(self::$validate_number, $this->data['dni'])) {
+                echo json_encode(ResponseHttp::status400('El campo dni solo admite numeros'));
+            } else if (!filter_var($this->data['email'], FILTER_VALIDATE_EMAIL)) {
+                echo json_encode(ResponseHttp::status400('Formato de correo incorrecto'));
+            } else if (!preg_match(self::$validate_rol, $this->data['rol'])) {
+                echo json_encode(ResponseHttp::status400('Rol invalido'));
+            } else if (strlen($this->data['password']) < 8 || strlen($this->data['confirmPassword']) < 8) {
+                echo json_encode(ResponseHttp::status400('La contraseña debe de tener un minimo de 8 caracteres'));
+            } else if ($this->data['password'] !== $this->data['confirmPassword']) {
+                echo json_encode(ResponseHttp::status400('las contraseñas no coinciden'));
+            } else {
+                new UserModel($this->data);
+                echo json_encode(UserModel::post());
+            }
+            exit;
+        }
+    }
 }
+
