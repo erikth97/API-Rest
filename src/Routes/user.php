1d888cbecf95c08c66227922ceb3e38e882177a3
Erik Tamayo <erik.tamayohe@uanl.edu.mx>
2023-05-25 12:07:13 -0600

    Update user.php

--- src/Routes/user.php
+++ src/Routes/user.php
@@ -3,18 +3,21 @@
 use App\Config\ResponseHttp;
 use App\Controllers\UserController;
 
-$method = strtolower($_SERVER['REQUEST_METHOD']);
-$route = $_GET['route'];
-$params = explode('/', $route);
-$data = json_decode(file_get_contents("php://input"),true);
-$headers = getallheaders();
+$method  =   strtolower($_SERVER['REQUEST_METHOD']);
+$route   =   $_GET['route'];
+$params  =   explode('/', $route);
+$data    =   json_decode(file_get_contents("php://input"),true);
+$headers =   getallheaders();
 
+/************** INSTANCIA DEL CONTROLADOR DE USUARIO ************************/ 
 $app = new UserController($method,$route,$params,$data,$headers);
 
+/************* RUTAS ********************************************************/
 $app->getAll('user/');
+$app->getUser("user/{$params[1]}") ;
+$app->postSave('user/');
+$app->patchPassword('user/password/');
+$app->deleteUser('user/');
 
-$app->getUser("user/{$params[1]}");
-
-$app->post('user/');
-
+/************ ERROR 404 ******************************************************/ 
 echo json_encode(ResponseHttp::status404());
