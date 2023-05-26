e1de73cd425c409647270e92a2442bfb973836c0
Erik Tamayo <erik.tamayohe@uanl.edu.mx>
2023-05-26 11:05:00 -0600

    Update ResponseHttp.php

--- src/Config/ResponseHttp.php
+++ src/Config/ResponseHttp.php
@@ -8,6 +8,48 @@ class ResponseHttp {
             'status' => '',
             'message' =>''
       );
+
+      //////////////////////// CORS PRODUCCION ////////////////////////////////////////
+      final public static function headerHttpPro($method, $origin)
+      {
+            if(!isset($origin)) {
+                  die(json_encode(ResponseHttp::status401('No tiene autorizacion para el consumo de esta API')));
+            }
+
+            $list = ['http://nibiru.test']; 
+
+            if(in_array($origin,$list)){
+
+            if($method == 'OPTIONS') {  
+            header("Access-Control-Allow-Origin: $origin");
+            header('Access-Control-Allow-Methods: GET,PUT,POST,PATCH,DELETE');
+            header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Authorization"); 
+            exit(0);
+            } else {
+            header("Access-Control-Allow-Origin: $origin");
+            header('Access-Control-Allow-Methods: GET,PUT,POST,PATCH,DELETE');
+            header("Allow: GET, POST, OPTIONS, PUT, PATCH , DELETE");
+            header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Authorization"); 
+            header('Content-Type: application/json'); 
+            }
+      } else {
+            die(json_encode(ResponseHttp::status401('No tiene autorizacion para consumir esta API')));
+      }     
+}
+
+      /////////////////////// CORS DE DESARROLLO //////////////////////////////////////////
+      final public static function headerHttpDev($method)
+      {
+            if($method == 'OPTIONS') {
+                  exit(0);
+            }
+
+            echo header("Access-Control-Allow-Origin: *");
+            echo header('Access-Control-Allow-Methods: GET,PUT,POST,PATCH,DELETE');
+            echo header("Allow: GET, POST, OPTIONS, PUT, PATCH , DELETE");
+            echo header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Authorization"); 
+            echo header('Content-Type: application/json'); 
+      }     
       public static function status200($res)
       {
             http_response_code(200);
